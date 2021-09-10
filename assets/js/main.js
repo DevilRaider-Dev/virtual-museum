406214045c1b2a6b377783923019da7c5a6d77ca
Fabian2411 <fabian.backhaus1@gmx.de>
2021-09-10 15:00:27 +0200

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,85 +11,74 @@ let slides = [
   "tenth slide",
 ];
 
-/* let records = [
-  { baseimageurl: "https://unsplash.it/600" },
-  { baseimageurl: "https://unsplash.it/601" },
-  { baseimageurl: "https://unsplash.it/602" },
-  { baseimageurl: "https://unsplash.it/603" },
-  { baseimageurl: "https://unsplash.it/604" },
-  { baseimageurl: "https://unsplash.it/605" },
-  { baseimageurl: "https://unsplash.it/606" },
-  { baseimageurl: "https://unsplash.it/606" },
-  { baseimageurl: "https://unsplash.it/607" },
-  { baseimageurl: "https://unsplash.it/608" },
-  { baseimageurl: "https://unsplash.it/609" },
-]; */
+const containerCaroussel = document.getElementById("containerCaroussel");
+const galleryImg = document.getElementById("galleryImg");
 
 fetch(
-  `https://api.harvardartmuseums.org/image?q=width:<800&apikey=2cf58dfc-f8e1-42b2-84af-24cda9d00b73`
+  `https://api.harvardartmuseums.org/image?q=width:<600&apikey=2cf58dfc-f8e1-42b2-84af-24cda9d00b73`
 )
   .then((response) => response.json())
   .then((data) => {
-    /* document.getElementById("containerCaroussel").innerHTML = `
-    <div id="carouselExampleControls" class="carousel slide carousel-fade" data-ride="carousel">
-    <div id="carousel-container" class="carousel-inner">
-    </div>
-  </div>`; */
-
-    for (i = 0; i < data.records.length; i++) {
-      //for (i = 0; i < records.length; i++) {
-      if (i < 10) {
-        if (i == 0) {
-          //set active
-          //document.getElementById("carousel-container").innerHTML += `<div class="carousel-item active"><img class="d-block w-100" src="${data.records[i].baseimageurl}" alt="${slides[i]}"></div>`;
-        } else {
-          //document.getElementById("carousel-container").innerHTML += `<div class="carousel-item"><img class="d-block w-100" src="${data.records[i].baseimageurl}" alt="${slides[i]}"></div>`;
-        }
-      }
-    }
-
-    /* document.getElementById("carousel-container").innerHTML += `<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>`; */
+    containerCaroussel.innerHTML = `<p>${data.records[2].date}</p>`
+    
+    <img id="galleryImg" src="" alt="">
+    galleryImg.setAttribute("src", data.records[2].baseimageurl);
   });
 
-function searchArt() {
-  let person = "displayname:" + document.getElementById("inputArtist").value;
-  //let year = "displayyear:" + document.getElementById("inputYear").value
-
-  fetch(
-    `https://api.harvardartmuseums.org/person?q=${person}&apikey=2cf58dfc-f8e1-42b2-84af-24cda9d00b73`
+async function next() {
+  let rnd = (Math.random() * 10).toFixed();
+  await fetch(
+    `https://api.harvardartmuseums.org/image?q=width:<600&apikey=2cf58dfc-f8e1-42b2-84af-24cda9d00b73`
   )
     .then((response) => response.json())
     .then((data) => {
-      if (data.records.length > 0) {
-        document.getElementById("displayname").innerHTML =
-          "Name: " + data.records[0].displayname;
-        document.getElementById("culture").innerHTML =
-          "Culture: " + data.records[0].culture;
-        document.getElementById("birth").innerHTML =
-          "Birth: " + data.records[0].datebegin;
-        document.getElementById("death").innerHTML =
-          "Death:" + data.records[0].dateend;
-        document.getElementById("birthplace").innerHTML =
-          "Birthplace: " + data.records[0].birthplace;
-        document
-          .getElementById("galleryURL")
-          .setAttribute("href", data.records[0].url);
-        document.getElementById("galleryURL").innerHTML = "Gallery";
-        document
-          .getElementById("wikidataURL")
-          .setAttribute(
-            "href",
-            "https://www.wikidata.org/wiki/" + data.records[0].url
-          );
-        document.getElementById("wikidataURL").innerHTML = "WikiData";
-      } else {
-      }
+      containerCaroussel.innerHTML = `<p>${data.records[rnd].date}</p>`
+      galleryImg.setAttribute("src", data.records[rnd].baseimageurl);
     });
+}
+
+async function previous() {
+  let rnd = (Math.random() * 10).toFixed();
+  await fetch(
+    `https://api.harvardartmuseums.org/image?q=width:<600&apikey=2cf58dfc-f8e1-42b2-84af-24cda9d00b73`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      containerCaroussel.innerHTML = `<p>${data.records[rnd].date}</p>`
+      galleryImg.setAttribute("src", data.records[rnd].baseimageurl);
+    });
+}
+
+function searchArt() {
+
+document.getElementById("containerGallery").style.display = "inline-block";
+
+let person = "displayname:" + document.getElementById("inputArtist").value;
+//let year = "displayyear:" + document.getElementById("inputYear").value
+
+fetch(
+  `https://api.harvardartmuseums.org/person?q=${person}&apikey=2cf58dfc-f8e1-42b2-84af-24cda9d00b73`
+)
+  .then((response) => response.json())
+  .then((data) => {
+    if (data.records.length > 0) {
+      document.getElementById("displayname").innerHTML =
+        data.records[0].displayname;
+      document.getElementById("culture").innerHTML = data.records[0].culture;
+      document.getElementById("birth").innerHTML = data.records[0].datebegin;
+      document.getElementById("death").innerHTML = data.records[0].dateend;
+      document.getElementById("birthplace").innerHTML =
+        data.records[0].birthplace;
+      document
+        .getElementById("galleryURL")
+        .setAttribute("href", data.records[0].url);
+      document
+        .getElementById("wikidataURL")
+        .setAttribute(
+          "href",
+          "https://www.wikidata.org/wiki/" + data.records[0].wikidata_id
+        );
+    } else {
+    }
+  });
 }
